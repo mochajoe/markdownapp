a462f9597c63e2d208e966ca317b8daddd09fa69
Anthony Nguyen <anthony@Xueyans-MBP.charter.com>
2018-11-12 23:31:55 -0800

    created textArea tag

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="editor">
-      </div>
+      <textarea id="editor">
+      </textarea>
     );
   }
 }
