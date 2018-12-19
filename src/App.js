90bfc44b1e348646b0553951a2420a88e5395c8e
mochajoe <anguyee@gmu.edu>
2018-12-18 21:30:00 -0800

    sdfsdf

--- src/App.js
+++ src/App.js
@@ -30,4 +30,4 @@ class App extends Component {
 
 export default App;
 
-//////working on making smaller componentsddddddsdfsdfsdfsfsdsssfdsfssssssddssssdfsddssssssdddsdfdsf
+//////working on making smaller componentsddddddsdfsdfsdfsfsdsssfdsfssssssddssssdfsddssdfsssssdddsdfdsf
